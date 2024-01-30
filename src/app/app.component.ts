4f52e5cdffe6e0f679cebf8e544a21bfe177bc5f
DGabrielP <dgpulgarin@sudamericano.edu.ec>
2024-01-30 17:51:57 -0500

    Hice una pagina desde cero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { CuerpoComponent } from './components/cuerpo/cuerpo.component';
+import { AnexosComponent } from './components/anexos/anexos.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, InicioComponent, CuerpoComponent, AnexosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/anexos/anexos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-anexos',
+  standalone: true,
+  imports: [],
+  templateUrl: './anexos.component.html',
+  styleUrl: './anexos.component.css'
+})
+export class AnexosComponent {
+
+}
